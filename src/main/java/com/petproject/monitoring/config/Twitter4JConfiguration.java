a45a1eb48dfe1007deb347f6cbbead1c81794df7
Nick Rybak <nrybak94@gmail.com>
2018-08-17 23:05:08 +0300

    deploy: prepared for deploying

--- src/main/java/com/petproject/monitoring/config/Twitter4JConfiguration.java
+++ src/main/java/com/petproject/monitoring/config/Twitter4JConfiguration.java
@@ -1,8 +1,8 @@
 package com.petproject.monitoring.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
@@ -10,20 +10,19 @@ import twitter4j.conf.ConfigurationBuilder;
 @Configuration
 public class Twitter4JConfiguration {
 
-    private final Environment env;
-
-    public Twitter4JConfiguration(Environment env) {
-        this.env = env;
-    }
+    @Value("${twitter4j.oauth.consumer-key}") String consumerKey;
+    @Value("${twitter4j.oauth.consumer-secret}") String consumerSecret;
+    @Value("${twitter4j.oauth.access-token}") String accessToken;
+    @Value("${twitter4j.oauth.access-token-secret}") String accessTokenSecret;
 
     @Bean
     public Twitter getTwitterInstance() {
 		ConfigurationBuilder cb = new ConfigurationBuilder();
 		cb.setDebugEnabled(true)
-		  .setOAuthConsumerKey(env.getProperty("twitter4j.oauth.consumer-key"))
-		  .setOAuthConsumerSecret(env.getProperty("twitter4j.oauth.consumer-secret"))
-		  .setOAuthAccessToken(env.getProperty("twitter4j.oauth.access-token"))
-		  .setOAuthAccessTokenSecret(env.getProperty("twitter4j.oauth.access-token-secret"));
+		  .setOAuthConsumerKey(consumerKey)
+		  .setOAuthConsumerSecret(consumerSecret)
+		  .setOAuthAccessToken(accessToken)
+		  .setOAuthAccessTokenSecret(accessTokenSecret);
 		TwitterFactory tf = new TwitterFactory(cb.build());
 
         return tf.getInstance();
